06cc22cb57558ce7b82b8df6a15706db1ca90b1e
DevExpressExampleBot <fakeEmail>
2026-01-29 16:51:59 +0400

    Source auto update [skip ci]

--- VB/Default.aspx.vb
+++ VB/Default.aspx.vb
@@ -6,27 +6,27 @@ Imports System.Web.UI
 Imports System.Web.UI.WebControls
 
 Partial Public Class _Default
-    Inherits System.Web.UI.Page
+	Inherits System.Web.UI.Page
 
-    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
-        If hf.Contains("isNewClicked") Then
-            If CBool(hf.Get("isNewClicked")) = True Then
-                gridView.SettingsEditing.Mode = DevExpress.Web.GridViewEditingMode.EditForm
-            Else
-                gridView.SettingsEditing.Mode = DevExpress.Web.GridViewEditingMode.Batch
-            End If
-        End If
-    End Sub
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+		If hf.Contains("isNewClicked") Then
+			If CBool(hf.Get("isNewClicked")) = True Then
+				gridView.SettingsEditing.Mode = DevExpress.Web.GridViewEditingMode.EditForm
+			Else
+				gridView.SettingsEditing.Mode = DevExpress.Web.GridViewEditingMode.Batch
+			End If
+		End If
+	End Sub
 
-    Protected Sub ASPxCallbackPanel1_Callback(ByVal sender As Object, ByVal e As DevExpress.Web.CallbackEventArgsBase)
-       If hf.Contains("isNewClicked") Then
-            If CBool(hf.Get("isNewClicked")) = True Then
-                gridView.SettingsEditing.Mode = DevExpress.Web.GridViewEditingMode.EditForm
-                hf.Set("isNewClicked", False)
-                gridView.AddNewRow()
-            Else
-                gridView.SettingsEditing.Mode = DevExpress.Web.GridViewEditingMode.Batch
-            End If
-       End If
-    End Sub
+	Protected Sub ASPxCallbackPanel1_Callback(ByVal sender As Object, ByVal e As DevExpress.Web.CallbackEventArgsBase)
+	   If hf.Contains("isNewClicked") Then
+			If CBool(hf.Get("isNewClicked")) = True Then
+				gridView.SettingsEditing.Mode = DevExpress.Web.GridViewEditingMode.EditForm
+				hf.Set("isNewClicked", False)
+				gridView.AddNewRow()
+			Else
+				gridView.SettingsEditing.Mode = DevExpress.Web.GridViewEditingMode.Batch
+			End If
+	   End If
+	End Sub
 End Class
